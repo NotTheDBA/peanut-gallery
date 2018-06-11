dd57645e3326b086bcb5042ee0c2f1049cbb37f8
Randy Reitz <rsreitz@outlook.com>
2018-06-11 12:35:04 -0400

    Scraping WaPo.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# peanut-gallery
-Peanut Gallery is a news aggregator and commenting portal.
-
+# Peanut Gallery
+Peanut Gallery is a news and comment gathering portal powered by Mongo, Express, Handlebars and Node.
 
 
 ### Setup
-Create a Heroku app in your project directory.
-Run this command in your Terminal/Bash window:
+* Clone the repo to your local machine
+* Run these commands in your Terminal/Bash window:
+```
+heroku create
+heroku addons:create mongolab
+git push heroku master
+```
+#### Heroku Demo
 
-    * `heroku addons:create mongolab`
-
-
-### Heroku Demo
-
-[Demo Site](https://arcane-citadel-12465.herokuapp.com/)
-[Scrape URL](https://arcane-citadel-12465.herokuapp.com/scrape)
-[Raw Articles](https://arcane-citadel-12465.herokuapp.com/articles)
+* [Demo Site](https://arcane-citadel-12465.herokuapp.com/)
+* [Scrape URL](https://arcane-citadel-12465.herokuapp.com/scrape)
+* [Raw Articles](https://arcane-citadel-12465.herokuapp.com/articles)
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,33 +7,41 @@ module.exports = function(app) {
     // A GET route for scraping the echoJS website
     app.get("/scrape", function(req, res) {
         // First, we grab the body of the html with request
-        axios.get("http://www.echojs.com/").then(function(response) {
+        axios.get("https://www.washingtonpost.com/national/?nid=top_nav_national").then(function(response) {
+            // console.log(response);
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
             // Now, we grab every h2 within an article tag, and do the following:
-            $("article h2").each(function(i, element) {
+            $("div .story-body").each(function(i, element) {
                 // Save an empty result object
+                // console.log(this)
+                // console.log($(this).children("a"));
                 var result = {};
 
-                // Add the text and href of every link, and save them as properties of the result object
+                // // // Add the text and href of every link, and save them as properties of the result object
                 result.title = $(this)
+                    .children("div .story-headline")
+                    .children("h3")
                     .children("a")
                     .text();
+
                 result.link = $(this)
+                    .children("div .story-headline")
+                    .children("h3")
                     .children("a")
                     .attr("href");
-
-                // Create a new Article using the `result` object built from scraping
-                db.Article.create(result)
-                    .then(function(dbArticle) {
-                        // View the added result in the console
-                        console.log(dbArticle);
-                    })
-                    .catch(function(err) {
-                        // If an error occurred, send it to the client
-                        return res.json(err);
-                    });
+                console.log(result)
+                    // // Create a new Article using the `result` object built from scraping
+                    // db.Article.create(result)
+                    //     .then(function(dbArticle) {
+                    //         // View the added result in the console
+                    //         console.log(dbArticle);
+                    //     })
+                    //     .catch(function(err) {
+                    //         // If an error occurred, send it to the client
+                    //         return res.json(err);
+                    //     });
             });
 
             // If we were able to successfully scrape and save an Article, send a message to the client
