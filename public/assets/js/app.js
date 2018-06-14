d1daeefecbb6ee0b02b1e1904aabd01c60133f78
Randy Reitz <rsreitz@outlook.com>
2018-06-14 08:47:45 -0400

    Articles now working with Handlebars.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,23 +1,23 @@
 // TODO:  re-write the content fillers to use handlebars
 
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
+// // Grab the articles as a json
+// $.getJSON("/articles", function(data) {
 
-    // var hbsObject = {
-    //     articles: data
-    // };
-    // res.render("index", hbsObject);
+//     // var hbsObject = {
+//     //     articles: data
+//     // };
+//     // res.render("index", hbsObject);
 
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
-});
+//     // For each one
+//     for (var i = 0; i < data.length; i++) {
+//         // Display the apropos information on the page
+//         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//     }
+// });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "p", function () {
     // Empty the notes from the note section
     $("#notes").empty();
     // Save the id from the p tag
@@ -25,11 +25,11 @@ $(document).on("click", "p", function() {
 
     // Now make an ajax call for the Article
     $.ajax({
-            method: "GET",
-            url: "/articles/" + thisId
-        })
+        method: "GET",
+        url: "/articles/" + thisId
+    })
         // With that done, add the note information to the page
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
             // The title of the article
             $("#notes").append("<h2>" + data.title + "</h2>");
@@ -51,23 +51,23 @@ $(document).on("click", "p", function() {
 });
 
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
 
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
-            method: "POST",
-            url: "/articles/" + thisId,
-            data: {
-                // Value taken from title input
-                title: $("#titleinput").val(),
-                // Value taken from note textarea
-                body: $("#bodyinput").val()
-            }
-        })
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            // Value taken from title input
+            title: $("#titleinput").val(),
+            // Value taken from note textarea
+            body: $("#bodyinput").val()
+        }
+    })
         // With that done
-        .then(function(data) {
+        .then(function (data) {
             // // Log the response
             // // console.log(data);
             // Empty the notes section
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,10 @@ module.exports = function (app) {
         res.render('index');
     });
 
-
-    app.get('/articlestest/:id', function (req, res) {
+    app.get('/articlestest/', function (req, res) {
 
         const db = require("../models");
-        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-        db.Article.find({ _id: req.params.id })
+        db.Article.find()
             // ..and populate all of the notes associated with it
             .populate("note")
             .then(function (dbArticle) {
@@ -36,4 +34,35 @@ module.exports = function (app) {
 
         // res.render('index');
     });
+
+    // app.get('/articlestest/:id', function (req, res) {
+
+    //     const db = require("../models");
+    //     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    //     db.Article.find({ _id: req.params.id })
+    //         // ..and populate all of the notes associated with it
+    //         .populate("note")
+    //         .then(function (dbArticle) {
+    //             // If we were able to successfully find an Article with the given id, send it back to the client
+    //             // res.json(dbArticle);
+
+    //             // console.log(dbArticle.title);
+
+    //             var hbsObject = {
+    //                 articles: dbArticle,
+    //                 layout: "new"
+    //             };
+    //             res.render('newidx', hbsObject);
+
+    //         })
+    //         .catch(function (err) {
+    //             // If an error occurred, send it to the client
+    //             // res.json(err);
+
+    //             console.log(err);
+    //         });
+
+
+    // res.render('index');
+// });
 }
